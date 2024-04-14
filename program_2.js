ad9f0ac8c1dbd4eaa1f3c4616d02edd9a4e08271
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-04-14 19:25:11 +0500

    Add files via upload

---
+++ program_2.js
@@ -0,0 +1,3 @@
+//current Date: 13-4-2024
+//This program will add two digits
+console.log(2 + 9);
---
+++ program_2.ts
@@ -0,0 +1,5 @@
+//current Date: 13-4-2024
+
+//This program will add two digits
+
+console.log(2+9);
