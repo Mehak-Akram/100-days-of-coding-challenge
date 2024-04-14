5aed1fa8562960c09d431b4402676d7b502853dc
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-04-14 19:26:21 +0500

    Add files via upload

---
+++ greetings.js
@@ -0,0 +1,5 @@
+var names = ["Mehak", "falza", "Misbha", "Muntaha"];
+var message = "Welcome to typescript class:  ";
+for (var i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
