5e16bbee7bd3eed4a17fe4db67d75ab8799d8c9e
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-04-14 19:24:28 +0500

    Add files via upload

---
+++ favorite_number.js
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+var favorite_Number = 7;
+var message = "Here is your favorite number" + favorite_Number;
+console.log(message);
