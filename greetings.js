0c1aeb744385a4bef381e2f744f0e3cb561f0da4
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-04-14 20:22:02 +0500

    Delete greetings.js

--- greetings.js
+++
@@ -1,5 +0,0 @@
-var names = ["Mehak", "falza", "Misbha", "Muntaha"];
-var message = "Welcome to typescript class:  ";
-for (var i = 0; i < names.length; i++) {
-    console.log(message + names[i]);
-}
