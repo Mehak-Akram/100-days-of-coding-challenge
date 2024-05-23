5b9265c69d3528c83d280fb314ccc7f1cd81e15e
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-23 23:47:37 +0500

    Delete favorite_number.ts

--- favorite_number.ts
+++
@@ -1,8 +0,0 @@
-//Favorite Number: Store your favorite number in a variable. Then, using that variable,
-// create a message that reveals your favorite number. Print that message.
-
-const favorite_Number: number = 7;
-
-const message: string = "Here is your favorite number" + favorite_Number;
-
-console.log(message);
