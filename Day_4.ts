28d428133987038a2fd7c7e3511d58a45a40a1aa
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-23 23:58:20 +0500

    Add files via upload

---
+++ Day_4.ts
@@ -0,0 +1,35 @@
+//Q_10
+
+//Adding Comments:
+ 
+//current Date: 13-4-2024
+
+//This program will right hello world
+console.log("Hello World");
+
+//current Date: 13-4-2024
+
+//This program will add two digits
+
+console.log(2+9);
+
+//Q_11
+
+let namess: string[] = ["Mehak","falza", "Misbha", "Muntaha"]
+
+for(let i=0; i < namess.length; i++){
+
+    console.log(namess[i]);
+}
+
+//Q_12
+
+let names: string[] = ["Mehak","falza", "Misbha", "Muntaha"]
+
+const message: string = "Welcome to typescript class:  "
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message+names[i]);
+}
+
