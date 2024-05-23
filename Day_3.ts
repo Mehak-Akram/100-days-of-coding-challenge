16c5b57488713704168616aefb1920ac9900b6ad
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-23 23:57:11 +0500

    Add files via upload

---
+++ Day_3.ts
@@ -0,0 +1,24 @@
+//Question_7
+
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//Be sure to enclose your operations in print statements to see the results.
+
+
+console.log(6+2); //Addition
+
+console.log(10-2); //subtraction
+
+console.log(4*2); //multipication
+
+console.log(16/2); //devide
+
+//Question_8
+
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+
+const favorite_Number: number = 7;
+
+const message: string = "Here is your favorite number" + favorite_Number;
+
+console.log(message);
