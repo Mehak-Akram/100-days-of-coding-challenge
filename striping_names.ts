fb6e5c6f196725c9bac991e992113bc9faf9e89f
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-23 23:35:28 +0500

    Delete striping_names.ts

--- striping_names.ts
+++
@@ -1,8 +0,0 @@
-//Stripping Names:
-
-
-const personName: string = "   \n\tMehak Naz\t\n   ";
-console.log(personName);
-
-const without_Whitespace : string = personName.trim();
-console.log(without_Whitespace);
