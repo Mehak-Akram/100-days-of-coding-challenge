60cbdad7e4baf1695111674bc658eb7fab548c43
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-23 23:56:18 +0500

    Delete greetings.ts

--- greetings.ts
+++
@@ -1,8 +0,0 @@
-let names: string[] = ["Mehak","falza", "Misbha", "Muntaha"]
-
-const message: string = "Welcome to typescript class:  "
-
-for(let i=0; i<names.length; i++){
-
-    console.log(message+names[i]);
-}
